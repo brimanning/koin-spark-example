a1974adcbaf152adc29b861f404dc1c4d13115f0
Brian Manning <bri@brimanning.com>
2019-10-16 11:10:28 -0400

    Simplify example

--- src/main/java/Application.kt
+++ src/main/java/Application.kt
@@ -1,11 +1,12 @@
 package com.brimanning.koinsparkexample
 
+import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
+import org.koin.core.inject
 import org.koin.dsl.module
 import spark.Spark.get
 
 fun main(args: Array<String>) {
-
     startKoin {
         printLogger()
         modules(
@@ -15,5 +16,9 @@ fun main(args: Array<String>) {
         )
     }
 
-    get("/") { _, _ -> SomeComponent().service.helloMessage() }
+    get("/") { _, _ -> SparkApplication().service.helloMessage() }
+}
+
+class SparkApplication : KoinComponent {
+    val service by inject<SomeService>()
 }
--- src/main/java/SomeComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.brimanning.koinsparkexample
-
-import org.koin.core.KoinComponent
-import org.koin.core.inject
-
-class SomeComponent : KoinComponent {
-
-    val service by inject<SomeService>()
-}
